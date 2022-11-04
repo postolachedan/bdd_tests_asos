914ee4330a5cb32f5ddf968e40739b63e2cb4615
Dan Postolache <dan_postolache@epam.com>
2022-11-04 14:49:34 +0200

    changed account button locator

--- src/main/java/org/example/pageobjects/pages/HomePage.java
+++ src/main/java/org/example/pageobjects/pages/HomePage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class HomePage extends BasePage {
-    @FindBy(xpath = "//span[@type='accountHalloweenUnfilled']")
+    @FindBy(xpath = "//span[@type='accountUnfilled']")
     WebElement accountButton;
     @FindBy(id = "chrome-search")
     WebElement searchField;
